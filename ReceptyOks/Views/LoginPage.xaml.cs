82c9ce734c0af3f254df668e5c5acfb0abbd155d
DariuszTom <dktomczak@o2.pl>
2026-01-18 15:38:34 +0100

    add login page

---
+++ ReceptyOks/Views/LoginPage.xaml.cs
@@ -0,0 +1,12 @@
+using ReceptyOks.ViewModels;
+
+namespace ReceptyOks.Views;
+
+public partial class LoginPage : ContentPage
+{
+	public LoginPage(LoginViewModel viemModel)
+	{
+		InitializeComponent();
+		BindingContext = viemModel;
+	}
+}
