b9df85df693e206ae83e4e78296fe9872f83da8e
DariuszTom <dktomczak@o2.pl>
2026-02-06 20:24:41 +0100

    Refactor RecipeDbContext formatting for readability
    Re-indented and reformatted entity configuration code in RecipeDbContext.cs for improved consistency and readability. No functional changes were made.

--- ReceptyOks.Api/Middleware/RecipeDbContext.cs
+++ ReceptyOks.Api/Middleware/RecipeDbContext.cs
@@ -5,14 +5,14 @@ namespace ReceptyOks.Api.Middleware;
 
 public class RecipeDbContext : DbContext
 {
-    public RecipeDbContext(DbContextOptions<RecipeDbContext> options) 
+    public RecipeDbContext(DbContextOptions<RecipeDbContext> options)
         : base(options) { }
 
     public DbSet<Recipe> Recipes => Set<Recipe>();
-      public DbSet<Category> Categories => Set<Category>();
-      public DbSet<Ingredient> Ingredients => Set<Ingredient>();
-      public DbSet<RecipeIngredient> RecipeIngredients => Set<RecipeIngredient>();
-      public DbSet<ShoppingListItem> ShoppingListItems => Set<ShoppingListItem>();
+    public DbSet<Category> Categories => Set<Category>();
+    public DbSet<Ingredient> Ingredients => Set<Ingredient>();
+    public DbSet<RecipeIngredient> RecipeIngredients => Set<RecipeIngredient>();
+    public DbSet<ShoppingListItem> ShoppingListItems => Set<ShoppingListItem>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -23,12 +23,12 @@ public class RecipeDbContext : DbContext
             entity.Property(r => r.Title).IsRequired().HasMaxLength(200);
             entity.Property(r => r.Image).HasColumnType("BLOB");
             entity.Property(r => r.ImageContentType).HasMaxLength(50);
-            
+
             entity.HasOne(r => r.Category)
                   .WithMany(c => c.Recipes)
                   .HasForeignKey(r => r.CategoryId)
                   .OnDelete(DeleteBehavior.SetNull);
-                  
+
             entity.HasIndex(r => r.UpdatedAt);
             entity.HasIndex(r => r.IsDeleted);
         });
@@ -52,43 +52,43 @@ public class RecipeDbContext : DbContext
         // RecipeIngredient (many-to-many)
         modelBuilder.Entity<RecipeIngredient>(entity =>
       {
-            entity.HasKey(ri => ri.Id);
+          entity.HasKey(ri => ri.Id);
 
-         entity.HasOne(ri => ri.Recipe)
-                  .WithMany(r => r.Ingredients)
-         .HasForeignKey(ri => ri.RecipeId)
-    .OnDelete(DeleteBehavior.Cascade);
+          entity.HasOne(ri => ri.Recipe)
+                   .WithMany(r => r.Ingredients)
+          .HasForeignKey(ri => ri.RecipeId)
+     .OnDelete(DeleteBehavior.Cascade);
 
-         entity.HasOne(ri => ri.Ingredient)
-      .WithMany(i => i.RecipeIngredients)
-        .HasForeignKey(ri => ri.IngredientId)
-       .OnDelete(DeleteBehavior.Cascade);
+          entity.HasOne(ri => ri.Ingredient)
+       .WithMany(i => i.RecipeIngredients)
+         .HasForeignKey(ri => ri.IngredientId)
+        .OnDelete(DeleteBehavior.Cascade);
 
-       entity.HasIndex(ri => new { ri.RecipeId, ri.IngredientId });
-    });
+          entity.HasIndex(ri => new { ri.RecipeId, ri.IngredientId });
+      });
 
         // ShoppingListItem
-      modelBuilder.Entity<ShoppingListItem>(entity =>
-     {
-       entity.HasKey(s => s.Id);
-            entity.Property(s => s.Name).IsRequired().HasMaxLength(200);
-entity.Property(s => s.Unit).HasMaxLength(50);
-            entity.Property(s => s.BoughtBy).HasMaxLength(100);
-            entity.Property(s => s.Note).HasMaxLength(500);
+        modelBuilder.Entity<ShoppingListItem>(entity =>
+       {
+           entity.HasKey(s => s.Id);
+           entity.Property(s => s.Name).IsRequired().HasMaxLength(200);
+           entity.Property(s => s.Unit).HasMaxLength(50);
+           entity.Property(s => s.BoughtBy).HasMaxLength(100);
+           entity.Property(s => s.Note).HasMaxLength(500);
 
-        entity.HasOne(s => s.Ingredient)
-    .WithMany()
-         .HasForeignKey(s => s.IngredientId)
-  .OnDelete(DeleteBehavior.SetNull);
+           entity.HasOne(s => s.Ingredient)
+     .WithMany()
+          .HasForeignKey(s => s.IngredientId)
+   .OnDelete(DeleteBehavior.SetNull);
 
-     entity.HasOne(s => s.Recipe)
-      .WithMany()
-      .HasForeignKey(s => s.RecipeId)
-          .OnDelete(DeleteBehavior.SetNull);
+           entity.HasOne(s => s.Recipe)
+          .WithMany()
+          .HasForeignKey(s => s.RecipeId)
+              .OnDelete(DeleteBehavior.SetNull);
 
-  entity.HasIndex(s => s.IsBought);
-            entity.HasIndex(s => s.UpdatedAt);
-       entity.HasIndex(s => s.IsDeleted);
-        });
+           entity.HasIndex(s => s.IsBought);
+           entity.HasIndex(s => s.UpdatedAt);
+           entity.HasIndex(s => s.IsDeleted);
+       });
     }
 }
