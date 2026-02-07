4b079e12942e44367e9f9b3e29cacd2fb3a1a792
DariuszTom <dktomczak@o2.pl>
2026-02-07 13:29:20 +0100

    add

--- ReceptyOks.Shared/GlobalUsings.cs
+++ ReceptyOks.Shared/GlobalUsings.cs
@@ -1,7 +1,4 @@
-﻿global using System;
-global using System.Collections.Generic;
-global using System.Text;
-global using System.Threading.Tasks;
+﻿global using System.Text;
 
 namespace ReceptyOks.Shared
 {
