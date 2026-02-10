793a0a571de4d889f47221d40e096b516db3936b
Dariusz <78443238+DariuszTom@users.noreply.github.com>
2026-02-10 13:01:47 +0100

    Setup Blazor viewer parameters and theme handling

--- ReceptyOks/Views/RecipeDetailPage.xaml.cs
+++ ReceptyOks/Views/RecipeDetailPage.xaml.cs
@@ -17,14 +17,25 @@ public partial class RecipeDetailPage : ContentPage
         _viewerState = viewerState;
         BindingContext = viewModel;
 
-        // Subscribe to ViewModel property changes to update Blazor component
+        SetupBlazorViewerParameters();
+
         viewModel.PropertyChanged += OnViewModelPropertyChanged;
+        Application.Current!.RequestedThemeChanged += OnRequestedThemeChanged;
+    }
+
+    private bool IsDarkTheme => Application.Current?.RequestedTheme == AppTheme.Dark;
+
+    private void SetupBlazorViewerParameters()
+    {
+        InstructionsRoot.Parameters = new Dictionary<string, object?>
+        {
+            { "IsDarkTheme", IsDarkTheme }
+        };
     }
 
     protected override void OnAppearing()
     {
         base.OnAppearing();
-        // Always update content - it will be queued if Blazor isn't ready yet
         UpdateInstructionsHtml();
     }
 
@@ -33,13 +44,28 @@ public partial class RecipeDetailPage : ContentPage
         base.OnDisappearing();
         _viewModel.PropertyChanged -= OnViewModelPropertyChanged;
         _isBlazorInitialized = false;
+
+        if (Application.Current is not null)
+        {
+            Application.Current.RequestedThemeChanged -= OnRequestedThemeChanged;
+        }
     }
 
     private void OnBlazorWebViewInitialized(object? sender, BlazorWebViewInitializedEventArgs e)
     {
         _isBlazorInitialized = true;
-        // Now that Blazor is ready, push content to the component
         UpdateInstructionsHtml();
+    }
+
+    private void OnRequestedThemeChanged(object? sender, AppThemeChangedEventArgs e)
+    {
+        MainThread.BeginInvokeOnMainThread(() =>
+        {
+            InstructionsRoot.Parameters = new Dictionary<string, object?>
+            {
+                { "IsDarkTheme", IsDarkTheme }
+            };
+        });
     }
 
     private void OnViewModelPropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
@@ -56,7 +82,6 @@ public partial class RecipeDetailPage : ContentPage
 
         MainThread.BeginInvokeOnMainThread(() =>
         {
-            // Always set content; HtmlViewerState will queue it until Blazor signals ready
             _viewerState.Content = html;
         });
     }
